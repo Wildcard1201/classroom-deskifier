fec571d39b444896d3e4762cc2451f7b59a5000f
Jason Fuhrer <jf.33@hotmail.com>
2022-10-06 08:57:19 -0400

    object scaffolding

---
+++ text-based/text-script.js
@@ -0,0 +1,15 @@
+/* 
+Sample student Object
+
+function Student(name, vision, avoid, prefer, academic) {
+    this.name = name;                       // student name
+    this.vision_pref = vision;              // vision-based distance preference
+    this.student_avoid = avoid;             // student to avoid sitting by
+    this.student_prefer = prefer;           // preferred student to sit by
+    this.academic = academic;               // academic tier 1-3
+}
+
+const Bob = new Student('Bob', 'near', 'Steve', 'null', '2')
+
+*/
+
